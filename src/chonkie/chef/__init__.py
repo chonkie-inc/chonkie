2616656fe11c37a1d59b71ba319710ea5ac82c34
chonk-lain <lain@chonkie.ai>
2025-09-27 01:13:26 +0100

    linting

--- src/chonkie/chef/__init__.py
+++ src/chonkie/chef/__init__.py
@@ -1,8 +1,8 @@
 """Chef module."""
 
 from .base import BaseChef
-from .tablechef import TableChef
 from .markdown import MarkdownChef
+from .tablechef import TableChef
 from .text import TextChef
 
-__all__ = ["BaseChef", "TextChef", "TableChef", "MarkdownChef"]
+__all__ = ["BaseChef", "MarkdownChef", "TextChef", "TableChef"]
--- tests/chef/test_chef.py
+++ tests/chef/test_chef.py
@@ -1,9 +1,6 @@
 
 """Tests for the chef module."""
 
-import io
-import os
-import tempfile
 from pathlib import Path
 from unittest.mock import mock_open, patch
 
