91f91cf1dfcccf06d1a473a9f21a96198efe1c42
chonk-lain <lain@chonkie.ai>
2025-12-12 20:14:56 +0100

    Refactor CLI commands and update utility exports

--- src/chonkie/cli/cli_utils.py
+++ src/chonkie/cli/cli_utils.py
@@ -4,8 +4,23 @@ import os
 
 import typer
 
-from chonkie import SemanticChunker
-from chonkie.utils import login as login_function
+from chonkie import Pipeline, SemanticChunker
+
+# from chonkie.utils import login as login_function
+
+# @app.command()
+# def login(
+#     api_key: str = typer.Option(
+#         ...,
+#         prompt=True,
+#         hide_input=True,
+#         help="Your API key for authentication",
+#     ),
+# ) -> None:
+#     """Login to Chonkie CLI with your API key."""
+#     login_function(api_key)
+
+
 
 app = typer.Typer(
     name="chonkie",
@@ -31,6 +46,10 @@ def chunk(
         "semantic",
         help="Chunking method to use",
     ),
+    store_in: str = typer.Option(
+        None,
+        help="Where to store the chunks",
+    ),
 ) -> None:
     """Chunk - A simple chunking command."""
     typer.echo("Chunking text...")
@@ -40,21 +59,59 @@ def chunk(
         with open(text, "r", encoding="utf-8") as f:
             text = f.read()
     chunks = chunking_maker.chunk(text)
-    for i, chunk in enumerate(chunks, 1):
-        typer.echo(f"Chunk {i}:\n{chunk}\n")
+    if store_in is None:
+        for i, chunk in enumerate(chunks, 1):
+            typer.echo(f"Chunk {i}:\n{chunk}\n")
+    else:
+        typer.echo(f"Storing chunks in {store_in}...")
+        typer.echo("Chunks stored successfully.")
 
 
 @app.command()
-def login(
-    api_key: str = typer.Option(
-        ...,
-        prompt=True,
-        hide_input=True,
-        help="Your API key for authentication",
+def pipeline(
+    text: str = typer.Argument(
+        "please provide a text between quotes or filepath", help="Text to chunk"
+    ),
+    fetcher: str = typer.Option(
+        "file",
+        help="Fetcher method to use",
+    ),
+    chef: str = typer.Option(
+        None,
+        help="Chef method to use",
+    ),
+    chunker: str = typer.Option(
+        "semantic",
+        help="Chunking method to use",
+    ),
+    refiner: str = typer.Option(
+        None,
+        help="Refiner method to use",
+    ),
+    handshaker: str = typer.Option(
+        None,
+        help="Handshaker method to use",
     ),
 ) -> None:
-    """Login to Chonkie CLI with your API key."""
-    login_function(api_key)
+    """Chunk - A simple chunking command."""
+    pipeline = Pipeline()
+    pipeline.fetch_from(fetcher, path=text)
+    if chef is not None:
+        pipeline.process_with(chef)
+    pipeline.chunk_with(chunker)
+    if refiner is not None:
+        pipeline.refine_with(refiner)
+    if handshaker is not None:
+        pipeline.store_in(handshaker)
+    doc = pipeline.run()
+    if isinstance(doc, list):
+        for d in doc:
+            for i, chunk in enumerate(d.chunks): # type: ignore
+                typer.echo(f"Chunk {i}:\n{chunk}\n")
+    else:
+        for i, chunk in enumerate(doc.chunks):
+            typer.echo(f"Chunk {i}:\n{chunk}\n")
+
 
 
 if __name__ == "__main__":
--- src/chonkie/utils/__init__.py
+++ src/chonkie/utils/__init__.py
@@ -1,7 +1,12 @@
 """Module for utility functions."""
 
-from ._api import load_token, login
+# from ._api import load_token, login
 from .hub import Hubbie
 from .viz import Visualizer
 
-__all__ = ["Hubbie", "Visualizer", "login", "load_token"]
+__all__ = [
+    "Hubbie",
+    "Visualizer",
+    # "login",
+    # "load_token",
+]
